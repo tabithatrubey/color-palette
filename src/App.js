214a3d1ce4d23ab647bad3aae94f154ffbf3b861
tabithatrubey <tabithatrubey@outlook.com>
2022-12-22 17:49:40 -0800

    Changes to color palette

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
+  const colors = [
+    "#9253a1",
+    "#f063a4",
+    "#2dc5f4",
+    "#fcee21",
+    "#f16164",
+    "#70327e",
+    "#a42963",
+    "#0b6a88",
+    "#f89e4f",
+    "#ec015a"
+  ]
+  const [background, setBackground] = useState("#071415")
+  const [currentColor, setCurrentColor] = useState(null)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setCurrentColor(null);
+    }, 3000);
+    return () => clearTimeout(timeoutId); 
+  }, [currentColor])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style ={{background : background}}>
+      {currentColor !== null && <h1>Current color {currentColor}</h1>}
+      <div className='container'>
+        {colors.map((color, index) => ( 
+          <div key={index} className='card'>
+            <div style = {{ 
+              background: color, 
+              filter: "brightness(85%)",
+              boxShadow: color === background ? "0 0 5px #000" : ""
+            }}
+            className='box' onClick={() => setBackground(color)}>
+              </div>
+              <CopyToClipboard text={`color: ${color};`}>
+                <p style = {{color: color === background ? "#fff" : color}}onClick={() => setCurrentColor(color)}>{color}</p>
+              </CopyToClipboard>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
